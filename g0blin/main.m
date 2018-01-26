a3be5f428abd2e914ea7e19958fa83983e86d326
Sticktron <sticktron@hotmail.com>
2018-01-26 14:36:56 -0500

    we have system() again

--- g0blin/main.m
+++ g0blin/main.m
@@ -8,9 +8,13 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#include <dlfcn.h>
+
+int (*gsystem)(const char *) = 0;
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        gsystem = dlsym(RTLD_DEFAULT,"system"); //thx tihmstar :)
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
