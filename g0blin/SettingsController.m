b34b3693d211fd1bede8ad66e42eaab571630e26
Sticktron <sticktron@hotmail.com>
2018-01-26 14:35:05 -0500

    added issues button to settings; changed layout a bit

--- g0blin/SettingsController.m
+++ g0blin/SettingsController.m
@@ -11,6 +11,7 @@
 @interface SettingsController ()
 @property (weak, nonatomic) IBOutlet UIButton *redditButton;
 @property (weak, nonatomic) IBOutlet UIButton *discordButton;
+@property (weak, nonatomic) IBOutlet UIButton *issuesButton;
 @end
 
 @implementation SettingsController
@@ -19,21 +20,25 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.redditButton.layer.cornerRadius = 6;
-    self.discordButton.layer.cornerRadius = 6;
-    
+    self.redditButton.layer.cornerRadius = 10;
+    self.discordButton.layer.cornerRadius = 10;
+    self.issuesButton.layer.cornerRadius = 10;
+
     [self.reinstallBootstrapSwitch setOn:NO animated:NO];
+}
+
+- (IBAction)goIssues:(UIButton *)sender {
+    NSURL *url = [NSURL URLWithString:@"http://github.com/sticktron/g0blin/issues"];
+    [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
 }
 
 - (IBAction)goReddit:(UIButton *)sender {
     NSURL *url = [NSURL URLWithString:@"http://reddit.com/r/jailbreak"];
-//    [[UIApplication sharedApplication] openURL:url];
     [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
 }
 
 - (IBAction)goDiscord:(UIButton *)sender {
     NSURL *url = [NSURL URLWithString:@"http://discord.gg/RZqpUfR"];
-    //    [[UIApplication sharedApplication] openURL:url];
     [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
 }
 
