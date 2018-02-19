fa5527861cedd63cb6303a252fcfd8f4cb14d941
Sticktron <sticktron@hotmail.com>
2018-02-19 10:28:35 -0500

    fixed offsets for iPhone SE 10.3.2 beta (14f5065b)

--- g0blin/offsets.m
+++ g0blin/offsets.m
@@ -1234,24 +1234,43 @@ iPad6,12 - iPad 5/2017 (Cellular)
         // 10.3.2 Beta
         if (!strcmp(osversion, "14F5065b"))
         {
-            OFFSET_ZONE_MAP                        = 0xfffffff007548478;
+//             OFFSET_ZONE_MAP                        = 0xfffffff007548478;
+//             OFFSET_KERNEL_MAP                      = 0xfffffff0075a4050;
+//             OFFSET_KERNEL_TASK                     = 0xfffffff0075a4048;
+//             OFFSET_REALHOST                        = 0xfffffff00752aba0;
+//             OFFSET_BZERO                           = 0xfffffff007081f80;
+//             OFFSET_BCOPY                           = 0xfffffff007081dc0;
+//             OFFSET_COPYIN                          = 0xfffffff0071806f4;
+//             OFFSET_COPYOUT                         = 0xfffffff0071808e8;
+//             OFFSET_ROOTVNODE                       = 0xfffffff0075a40b0;
+//             OFFSET_CHGPROCCNT                      = 0xfffffff00738d944;
+//             OFFSET_KAUTH_CRED_REF                  = 0xfffffff007367cf4;
+//             OFFSET_IPC_PORT_ALLOC_SPECIAL          = 0xfffffff007099e94;
+//             OFFSET_IPC_KOBJECT_SET                 = 0xfffffff0070ad16c;
+//             OFFSET_IPC_PORT_MAKE_SEND              = 0xfffffff0070999b8;
+//             OFFSET_IOSURFACEROOTUSERCLIENT_VTAB    = 0xfffffff006e849f8;
+//             OFFSET_ROP_ADD_X0_X0_0x10              = 0xfffffff006482174;
+//             OFFSET_OSSERIALIZER_SERIALIZE          = 0xfffffff007440ad0;
+//             OFFSET_ROP_LDR_X0_X0_0x10              = 0xfffffff0063d0a84;            
+            
+            OFFSET_ZONE_MAP                        = 0xfffffff007085050;
             OFFSET_KERNEL_MAP                      = 0xfffffff0075a4050;
             OFFSET_KERNEL_TASK                     = 0xfffffff0075a4048;
             OFFSET_REALHOST                        = 0xfffffff00752aba0;
             OFFSET_BZERO                           = 0xfffffff007081f80;
             OFFSET_BCOPY                           = 0xfffffff007081dc0;
-            OFFSET_COPYIN                          = 0xfffffff0071806f4;
-            OFFSET_COPYOUT                         = 0xfffffff0071808e8;
+            OFFSET_COPYIN                          = 0xfffffff007180604;
+            OFFSET_COPYOUT                         = 0xfffffff0071807f8;
             OFFSET_ROOTVNODE                       = 0xfffffff0075a40b0;
-            OFFSET_CHGPROCCNT                      = 0xfffffff00738d944;
-            OFFSET_KAUTH_CRED_REF                  = 0xfffffff007367cf4;
-            OFFSET_IPC_PORT_ALLOC_SPECIAL          = 0xfffffff007099e94;
-            OFFSET_IPC_KOBJECT_SET                 = 0xfffffff0070ad16c;
-            OFFSET_IPC_PORT_MAKE_SEND              = 0xfffffff0070999b8;
+            OFFSET_CHGPROCCNT                      = 0xfffffff00738d7e0;
+            OFFSET_KAUTH_CRED_REF                  = 0xfffffff007367b78;
+            OFFSET_IPC_PORT_ALLOC_SPECIAL          = 0xfffffff007099e3c;
+            OFFSET_IPC_KOBJECT_SET                 = 0xfffffff0070ad094;
+            OFFSET_IPC_PORT_MAKE_SEND              = 0xfffffff007099960;
             OFFSET_IOSURFACEROOTUSERCLIENT_VTAB    = 0xfffffff006e849f8;
-            OFFSET_ROP_ADD_X0_X0_0x10              = 0xfffffff006482174;
-            OFFSET_OSSERIALIZER_SERIALIZE          = 0xfffffff007440ad0;
-            OFFSET_ROP_LDR_X0_X0_0x10              = 0xfffffff0063d0a84;
+            OFFSET_ROP_ADD_X0_X0_0x10              = 0xfffffff006481174;
+            OFFSET_OSSERIALIZER_SERIALIZE          = 0xfffffff00744090c;
+            OFFSET_ROP_LDR_X0_X0_0x10              = 0xfffffff0063d0a84;            
         }
         
         // 10.3.1
